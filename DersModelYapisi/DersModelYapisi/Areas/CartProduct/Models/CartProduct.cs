c88ed9b54c97a828dfcd45ef101903f6cf9c5d02
argunriza <argunrizaa@gmail.com>
2023-06-30 17:52:11 +0300

    Gorev-8

---
+++ DersModelYapisi/DersModelYapisi/Areas/CartProduct/Models/CartProduct.cs
@@ -0,0 +1,13 @@
+﻿using DersModelYapisi.Models;
+
+namespace DersModelYapisi.Areas.CartProduct.Models
+{
+    public class CartProduct
+    {
+        public int Id { get; set; }
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
